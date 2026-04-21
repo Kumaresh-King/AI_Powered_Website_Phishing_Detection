9532a4297d7ce4915722b65e93c019ef7fcdca65
Kumaresh-King <kumareshsri77@gmail.com>
2026-04-21 12:29:54 +0300

    fixed logs route and server start

--- backend/api.py
+++ backend/api.py
@@ -124,7 +124,7 @@ def get_logs():
 
 
 # 🚀 START SERVER (Render compatible)
-
+import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
